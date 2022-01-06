151edec1c6407381e2fe3cae2a8d941c94203684
John Bryan Valle <jbvalle.jv@gmail.com>
2022-01-06 17:02:51 +0100

    updated UI

--- main.c
+++ main.c
@@ -11,7 +11,13 @@ typedef struct {
 }cdatas_t;
 
 typedef struct {
-
+    //filter_args_t args[0]...Parameter für Datum
+    //filter_args_t args[1]...Parameter für t_min
+    //filter_args_t args[2]...Parameter für t_max
+    //filter_args_t args[3]...Parameter für niederschlag
+    //methode...Filtermethode
+    //arg1...limit1
+    //arg2...limit2
     int method;
     double arg1;
     double arg2;
@@ -545,13 +551,13 @@ void show_data(cdatas_t *cdata, int number_datasets){
 
     printf("+------------------------+\n");
     printf("| Ausgabeoptionen:       |\n");
-    printf("|* Datum                 |\n");
+    printf("|* datum                 |\n");
     printf("|* t_min                 |\n");
     printf("|* t_max                 |\n");
-    printf("|* Niederschlag          |\n");
+    printf("|* niederschlag          |\n");
     printf("|* gesamt                |\n");
     printf("|                        |\n");
-    printf("| z.B Datum, t_max       |\n");
+    printf("| z.B datum, t_max       |\n");
     printf("+------------------------+\n\n");
     printf("Eingabe: ");
 
@@ -562,10 +568,10 @@ void show_data(cdatas_t *cdata, int number_datasets){
     while(token != NULL){
 
         if(strcmp(token,"gesamt")==0)for(int i = 0; i < 4; i++)input[i] = 1;
-        if(strcmp(token,"Datum")==0)input[0] = 1;
+        if(strcmp(token,"datum")==0)input[0] = 1;
         if(strcmp(token,"t_min")==0)input[1] = 1;
         if(strcmp(token,"t_max")==0)input[2] = 1;
-        if(strcmp(token,"Niederschlag")==0)input[3] = 1;
+        if(strcmp(token,"niederschlag")==0)input[3] = 1;
 
         token = strtok(NULL," ,\n\t");
     }
@@ -585,7 +591,107 @@ void show_data(cdatas_t *cdata, int number_datasets){
     }
 }
 
-void get_input(filter_args_t *args, int *filter_options){
+void request_datum_args(filter_args_t *args){
+
+    
+    char *token, letter, buff[50], key[5];
+    //Input dient zur fall unterscheidung und zur "aktivierung" der auswahloption
+    int input[] = {0,0,0,0};
+    //Variablen fuer die eingabe eines zeitraums
+    int dd1,mm1,yy1;
+    int dd2,mm2,yy2;
+
+    //Initialisiere input buffer -> Benutzer eingabe
+    for(int i = 0; i < 50; i++)buff[i] = '\0';
+
+    printf("+-------------------------------------------------------------------+\n");
+    printf("| Filterung nach Datum: (Datensaetze vom 01.01.1948 - 31.12.2009)   |\n");
+    printf("|                                                                   |\n");
+    printf("| Bitte waehlen Sie eines der moeglichen Filtermethoden aus:        |\n");
+    printf("|* datum TT.MM.JJJJ - TT.MM.JJ           ->Filterung eines Zeitraums|\n");
+    printf("|* bis TT.MM.JJJJ                        ->alle Daten bis ...       |\n"); 
+    printf("|* ab TT.MM.JJJJ                         ->alle Daten ab  ...       |\n");
+    printf("|* 1. letzten X Tage                                                |\n");
+    printf("|* 2. letzten X Tage bis TT.MM.JJJJ                                 |\n");
+    printf("|                                                                   |\n");
+    printf("| z.B. datum 01.01.1995 - 01.01.2000                                |\n");
+    printf("| z.B. bis 01.01.1950                                               |\n");
+    printf("| z.B. ab 10.10.2005                                                |\n");
+    printf("| z.B. 1. letzten 50 Tage                                           |\n");
+    printf("| z.B. 2. letzten 100 Tage bis 10.10.2019                           |\n");
+    printf("+-------------------------------------------------------------------+\n\n");
+    printf("Eingabe: ");
+
+    //Zeichenweises einlesen
+    for(int i = 0; (letter = getchar()) != '\n';i++)buff[i] = letter;
+
+    //Einlesen vom schluesselwort zur fallunterscheidung
+    sscanf(buff,"%s",key);
+
+    //Array zum auffangen vom key
+    char dump[5];
+
+    //Wenn key == datum dann 1. Filteroption ausführen
+    if(strcmp(key, "datum")==0){
+        
+        //Entspricht filtermethode 2 filter_datum(2,limit1,limit2)
+        sscanf(buff, "%s %d.%d.%d - %d.%d.%d", dump,&dd1, &mm1, &yy1,&dd2,&mm2,&yy2);
+        //Filtermethode 2 weahlen
+        args[0].method = 2;
+        //mm,dd,yy in das format YYYYMMDD umwandeln
+        args[0].arg1 = (yy1 * 10000 + mm1 * 100 + dd1);
+        args[0].arg2 = (yy2 * 10000 + mm2 * 100 + dd2);
+    }
+
+
+    if(strcmp(token, "bis")  ==0){
+        printf("2");
+    }
+
+    if(strcmp(token, "ab")   ==0)printf("3");
+
+    if(strcmp(token, "1")    ==0)printf("4");
+
+    if(strcmp(token, "2")    ==0)printf("5");
+
+}
+
+void get_input(filter_args_t *args, int *filter_option){
+
+    char *token, letter, buff[50];
+
+    //Initialisiere input buffer -> Benutzer eingabe
+    for(int i = 0; i < 50; i++)buff[i] = '\0';
+
+
+    printf("+---------------------------------------------------+\n");
+    printf("| Bitten geben Sie die gewuenschten Filter ein      |\n");
+    printf("|* datum            ->Filterung nach Datum          |\n");
+    printf("|* t_min            ->Filterung nach t_min          |\n"); 
+    printf("|* t_max            ->Filterung nach t_max          |\n");
+    printf("|* niederschlag     ->Filterung nach niederschlag   |\n");
+    printf("|                                                   |\n");
+    printf("| z.B. datum, t_max, niederschlag                   |\n");
+    printf("+---------------------------------------------------+\n\n");
+    printf("Eingabe: ");
+
+    for(int i = 0; (letter = getchar()) != '\n';i++)buff[i] = letter;
+    
+    token = strtok(buff," ,\n\t");
+    
+    while(token != NULL){
+
+        if(strcmp(token,"datum")==0){
+            //Auffüllen der Parameter Daten mit Datum Filter Werten
+            filter_option[0] = 1;
+            request_datum_args(args);
+        }
+        if(strcmp(token,"t_min")==0)filter_option[1] = 1;
+        if(strcmp(token,"t_max")==0)filter_option[2] = 1;
+        if(strcmp(token,"niederschlag")==0)filter_option[3] = 1;
+
+        token = strtok(NULL," ,\n\t");
+    }
 
 
 }
@@ -593,6 +699,8 @@ void get_input(filter_args_t *args, int *filter_options){
 
 int main(void){
     
+    //Filteroptionen dient der "aktivierung" der filter
+    int filter_option[4] = {0,0,0,0};
     FILE *input;
     int number_datasets = 0, padding = 16;
     //determine number of datasets -> number_datasets
@@ -612,12 +720,15 @@ int main(void){
     input = fopen("input.csv","r");
     parse_input_to_struct(input, cdata, padding);
 
-    filter_datum(cdata, &number_datasets, 3, 10, 20091226);
-    //filter_tmax(cdata, &number_datasets, 1, 8, 30);
-    //filter_tmin(cdata, &number_datasets, 1, 3, 30);
-    //filter_niederschlag(cdata, &number_datasets, 1, 5, 30);
+    get_input(args, filter_option);
+
+    if(filter_option[0])filter_datum(cdata, &number_datasets, args[0].method, args[0].arg1, args[0].arg2);
+    //if(filter_option[1])filter_tmax(cdata, &number_datasets, 1, 8, 30);
+    //if(filter_option[2])filter_tmin(cdata, &number_datasets, 1, 3, 30);
+    //if(filter_option[3])filter_niederschlag(cdata, &number_datasets, 1, 5, 30);
     
     show_data(cdata, number_datasets);
+
     free(cdata);
     return 0;
 }
